ba28ebdedf5b26a7a58ea225d26ee3b69b3399ef
dems 1 <galkmat@hotmail.com>
2024-04-07 15:50:14 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,33 @@
+﻿// ConsoleApplication5.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <cmath>
+
+class Vector
+{
+private: 
+    double x;
+    double y;
+    double z;
+public:
+    Vector() : x(2), y(4), z(6)
+    {}
+    void Show()
+    {
+        std::cout << "\n" << x << " " << y << " " << z;
+    }
+    double longV()
+    {
+        return sqrt(x * x + y * y + z * z);
+    }
+
+};
+
+int main()
+{
+    Vector line;
+    line.Show();
+    std::cout << "\n" << line.longV();
+}
+
